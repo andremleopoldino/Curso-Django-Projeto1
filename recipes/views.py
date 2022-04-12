ad8ffb6204b0a09b646413f3700ff2cf4d2f0698
Andre Moreira Leopoldino <andrem01310@gmail.com>
2022-04-12 20:13:04 -0300

    Editando HTML/CSS

--- recipes/views.py
+++ recipes/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html')
 
 def contato(request):
     return HttpResponse('contato')
